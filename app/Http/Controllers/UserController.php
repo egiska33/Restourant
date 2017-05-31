0e7292dc3d907cb41f276bcf028082ebba045294
egiska33 <esivickas@gmail.com>
2017-05-31 20:02:30 +0300

    change ccs for navbar and profile view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getProfile()
     {
         $orders = Auth::user()->orders;
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div id="user_profile">
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -40,5 +40,5 @@
             </div>
         </div>
     </div>
-
+</div>
 @endsection
